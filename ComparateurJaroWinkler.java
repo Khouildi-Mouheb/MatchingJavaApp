c405bc6d7e727490ef1fac615f1799256fef8064
Mouheb <mouheb.khouildi@etudiant-enit.utm.tn>
2025-05-07 20:18:54 +0100

    comparateurJX and .xml file

---
+++ ComparateurJaroWinkler.java
@@ -0,0 +1,16 @@
+
+/*
+import org.apache.commons.text.similarity.JaroWinklerSimilarity;
+
+public class ComparateurJaroWinkler implements ComparateurNoms {
+    @Override
+    public double comparer(Nom nom1, Nom nom2) {
+        if (nom1 != null && nom2 != null) {
+            JaroWinklerSimilarity jw = new JaroWinklerSimilarity();
+            return jw.apply(nom1.getNom(), nom2.getNom()); // Compare names
+        } else {
+            return 0.0; // Handle null values
+        }
+    }
+}
+*/
