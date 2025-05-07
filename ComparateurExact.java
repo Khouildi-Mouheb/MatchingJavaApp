220709c9c5d4dc4253a6bd60377c505ea3c25dea
Mouheb <mouheb.khouildi@etudiant-enit.utm.tn>
2025-05-07 19:46:49 +0100

    optimizing the "comparer" method inside "ComparateurExact" class

--- ComparateurExact.java
+++ ComparateurExact.java
@@ -1,7 +1,11 @@
 public class ComparateurExact implements Comparateur {
     @Override
     public double comparer(String nom1, String nom2) {
-        return nom1.equals(nom2) ? 1.0 : 0.0;
+        if (nom1 != null && nom2 != null) {
+            return nom1.equals(nom2) ? 1.0 : 0.0;
+        } else {
+            return 0.0;
+        }
     }
 
 }
