c5abda7c6ea5adf3d85e73435fc5ba83c3e0ad96
yacine-boujelbenne <yacineboujelbenne@gmail.com>
2025-05-09 20:14:40 +0100

    add the slicer of the big lists

--- Main.java
+++ Main.java
@@ -10,15 +10,67 @@ public class Main {
         // içi j'ajoute un exemple de recuperateur qui fonctionne avec une liste static
         Recuperateur recuperateur = new RecuperateurStatique();
         Recuperateur recuperateur2 = new RecuperateurCSV(
-                "C:\\Users\\win10\\OneDrive\\Bureau\\work\\ENIT\\JAVA\\peps_names_1k.csv");
+                "C:\\Users\\win10\\OneDrive\\Bureau\\work\\ENIT\\JAVA\\peps_names_658k.csv");
 
         List<Nom> listeNoms = recuperateur.importData();
         List<Nom> listeNoms2 = recuperateur.importData();
         List<Nom> listeNoms3 = recuperateur2.importData();
-        for (Nom nom : listeNoms3) {
-            System.out.println("Nom: " + nom.getNom() + ", id: " + nom.getId());
-        }
 
+        /*
+         * for (Nom nom : listeNoms3) {
+         * System.out.println("Nom: " + nom.getNom() + ", id: " + nom.getId());
+         * }
+         */
+
+        if ((listeNoms3.size() > 2000)) {
+            int j = 0;
+
+            while (j + 1000 < listeNoms3.size() - 1001) {
+                List<Nom> listeNoms5 = listeNoms3.subList(j, j + 1000);
+                j = j + 1000;
+                if (j + 1000 > listeNoms3.size() - 1001) {
+                    listeNoms5 = listeNoms3.subList(j, listeNoms3.size() - 1);
+                }
+                // Créer une instance de GenerateurDeCondidat
+                GenerateurDeCondidat generateur = new GenerateurParTaille();
+
+                // Créer un CompositionneurDeNom
+                CompositionneurDeNom compositionneur = new CompositionneurStandard();
+
+                // Créer des instances de ComparateurNoms et ComparateurDeChaine
+                Comparateur comparateurExact = new ComparateurExact();
+                ComparateurNoms comparateur = new ComparateurNomsSimple(comparateurExact);
+
+                // Créer une instance du moteur de matching
+                // on doit ajouter une autre atribus pour le moteur qui est la liste vièrge
+                MoteurDeMatching moteur = new MoteurDeMatching(pretraiteur, comparateur, generateur);
+
+                System.out.println("-------------------Matching_App------------------");
+                System.out.println("-------------------------------------------------");
+                System.out.println("-La Comparaison entre les noms est en cours-");
+                // Appeler la méthode de test pour ComparateurExact
+                List<CoupleDenomAvecScore> listeNoms4 = moteur.comparer(listeNoms5, listeNoms5);
+
+                System.out.println("-------------------------------------------------");
+                System.out.println("-------------------------------------------------");
+                System.out.println("-La recherche entre les noms est en cours-");
+
+                // Appeler la méthode de test pour effectuer le matching
+                // List<CoupleDenomAvecScore> resultat = moteur.rechercher(listeNoms3);
+                System.out.println("-------------------------------------------------");
+                System.out.println("-------------------------------------------------");
+                System.out.println("-Affichage de la resultat du recherche apres la selection: -");
+
+                // Afficher les résultats du matching avec le Selectionneur
+                // resultat = selectionneur.selectionner(resultat);
+                listeNoms4 = selectionneur.selectionner(listeNoms4);
+                for (CoupleDenomAvecScore couple : listeNoms4) {
+                    System.out.println("Nom1: " + couple.getCouple().getNom() + " Nom2: " + couple.getCouple().getNom2()
+                            + ", Score: " + couple.getScore());
+                }
+
+            }
+        }
         // Créer une instance de GenerateurDeCondidat
         GenerateurDeCondidat generateur = new GenerateurParTaille();
 
