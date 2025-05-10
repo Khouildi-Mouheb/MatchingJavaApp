c8a17bdd24336622d6694e728f25e2f712b869f5
Mouheb <mouheb.khouildi@etudiant-enit.utm.tn>
2025-05-10 10:09:41 +0100

    -adding the "App" class to do my tests (mouheb) .

--- Main.java
+++ Main.java
@@ -119,4 +119,5 @@ public class Main {
         // couple.getScore());
         // }
     }
+    
 }
--- MoteurDeMatchingMouheb.java
+++ MoteurDeMatchingMouheb.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MoteurDeMatchingMouheb {
+    // recuperation de donnees
+
+    // instance d'un recuperateur csv
+    private RecuperateurCSV recuperateur;
+    // les noms sont dans la liste
+    List<Nom> listeNoms;
+
+    public MoteurDeMatchingMouheb() {
+        recuperateur = new RecuperateurCSV("C:\\Users\\khoui\\OneDrive\\Desktop\\peps_names_1k.csv");
+        listeNoms = recuperateur.importData();
+    }
+
+    public void affichage(List<Nom> listeNoms) {
+        for (Nom nom : listeNoms) {
+            System.out.println("Nom: " + nom.getNom() + ", id: " + nom.getId());
+        }
+    }
 
 }
