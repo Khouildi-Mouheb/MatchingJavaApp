eb00755d3eba59bfbb9ec99bb983507668c31da5
yacine-boujelbenne <yacineboujelbenne@gmail.com>
2025-05-13 22:42:12 +0100

    un peu de reglage

--- ComparateurLevenshtein.java
+++ ComparateurLevenshtein.java
@@ -1,9 +1,8 @@
 //Compte le nombre de modifications minimales (insertion, suppression, substitution) nécessaires pour transformer une chaîne en une autre.
 //Utilisé pour la correction orthographique et la détection de variations.
 
-
 //using AI
-public class LevenshteinComparateur implements Comparateur {
+public class ComparateurLevenshtein implements Comparateur {
 
     @Override
     public double comparer(String nom1Nettoye, String nom2Nettoye) {
@@ -12,16 +11,18 @@ public class LevenshteinComparateur implements Comparateur {
         int[][] dp = new int[len1 + 1][len2 + 1];
 
         // Initialisation de la matrice
-        for (int i = 0; i <= len1; i++) dp[i][0] = i;
-        for (int j = 0; j <= len2; j++) dp[0][j] = j;
+        for (int i = 0; i <= len1; i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= len2; j++)
+            dp[0][j] = j;
 
         // Calcul de la distance de Levenshtein
         for (int i = 1; i <= len1; i++) {
             for (int j = 1; j <= len2; j++) {
                 int cost = (nom1Nettoye.charAt(i - 1) == nom2Nettoye.charAt(j - 1)) ? 0 : 1;
                 dp[i][j] = Math.min(Math.min(
-                                dp[i - 1][j] + 1,   // Suppression
-                                dp[i][j - 1] + 1),  // Insertion
+                        dp[i - 1][j] + 1, // Suppression
+                        dp[i][j - 1] + 1), // Insertion
                         dp[i - 1][j - 1] + cost); // Substitution
             }
         }
@@ -29,6 +30,5 @@ public class LevenshteinComparateur implements Comparateur {
         // Retourne la distance de Levenshtein normalisée
         return 1 - ((double) dp[len1][len2] / Math.max(len1, len2));
     }
-
 
 }
--- Main3.java
+++ Main3.java
@@ -8,26 +8,28 @@ public class Main3 {
         Selectionneur selectionneur = new SelectionneurParSeuilSimple(0.0);
         GenerateurDeCondidat generateur = new GenerateurParTaille();
         Comparateur comparateurExact = new ComparateurExact();
+        Comparateur comparateurLv = new ComparateurLevenshtein();
         CompositionneurDeNom compositionneur = new CompositionneurStandard();
-        ComparateurNoms comparateur = new ComparateurNomsSimple(comparateurExact);
+        ComparateurNoms comparateur = new ComparateurNomsSimple(comparateurLv);
         ComparateurNoms comparateurComposition = new ComparteurNomsParComposition(compositionneur, comparateurExact);
 
         MoteurDeMatching moteur = new MoteurDeMatching(pretraiteur, comparateur, generateur);
 
         Recuperateur recuperateur = new RecuperateurCSV(
-                "C:\\Users\\win10\\OneDrive\\Bureau\\work\\ENIT\\JAVA\\peps_names_100.csv");
+                "C:\\Users\\win10\\OneDrive\\Bureau\\work\\ENIT\\JAVA\\peps_names_1k.csv");
         Recuperateur recuperateur2 = new RecuperateurCSV(
-                "C:\\Users\\win10\\OneDrive\\Bureau\\work\\ENIT\\JAVA\\peps_names_100.csv");
+                "C:\\Users\\win10\\OneDrive\\Bureau\\work\\ENIT\\JAVA\\peps_names_1k.csv");
 
         List<Nom> listeNoms1 = recuperateur.importData();
         List<Nom> listeNoms2 = recuperateur2.importData();
 
         System.out.println("-------------------------------------------------");
-        List<Nom> listeNoms3 = moteur.dedupliquer(listeNoms2, selectionneur);
+        List<CoupleDenomAvecScore> listeNoms3 = moteur.rechercher(listeNoms2.get(0), listeNoms1);
         int i = 0;
-        for (Nom couple : listeNoms3) {
+        for (CoupleDenomAvecScore couple : listeNoms3) {
 
-            System.out.println(couple.getNom() + " " + couple.getId() + i);
+            System.out.println(
+                    couple.getCouple().getNom1() + " : " + couple.getCouple().getNom2() + " " + couple.getScore() + i);
             i++;
         }
 
